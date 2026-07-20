9b1dac4570a7f4a275308f2068f2798b3ceb2b03
Evrins Hu <evrins.hu@gmail.com>
2026-07-20 13:36:23 +0800

    feat: stop tracing dashboard

--- ClashFX/Resources/dashboard/config.js
+++
@@ -1,6 +0,0 @@
-window.__METACUBEXD_CONFIG__ = {
-  ...(window.__METACUBEXD_CONFIG__ || {}),
-  defaultBackendURL:
-    (window.__METACUBEXD_CONFIG__ && window.__METACUBEXD_CONFIG__.defaultBackendURL) ||
-    `${window.location.protocol}//${window.location.host}`,
-}
